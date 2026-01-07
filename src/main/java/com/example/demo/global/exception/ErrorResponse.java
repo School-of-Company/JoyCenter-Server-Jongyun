3dbfc7a6a6fd3af14b1902d2b1c794c1cd42076b
C0ng_yun <s25069@gsm.hs.kr>
2026-01-07 10:33:57 +0900

    add: 공통 예외 응답용 ErrorResponse DTO 추가

---
+++ src/main/java/com/example/demo/global/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+
+}
