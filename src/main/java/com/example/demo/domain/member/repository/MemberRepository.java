31e9fdca44a1c0adf1f426aba527f3e3351b5edd
C0ng_yun <s25069@gsm.hs.kr>
2026-01-08 23:39:33 +0900

    add: MemberRepository 추가

---
+++ src/main/java/com/example/demo/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.member.repository;
+
+import com.example.demo.domain.member.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, String> {
+    Optional<MemberEntity> findByEmail(String email);
+}
