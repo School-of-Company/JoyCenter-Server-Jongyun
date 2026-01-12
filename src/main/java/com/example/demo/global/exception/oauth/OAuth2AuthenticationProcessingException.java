36d4419cce59e115761fdfa09e7c3aa8a362d747
C0ng_yun <s25069@gsm.hs.kr>
2026-01-12 23:46:55 +0900

    add : OAuth2AuthenticationProcessingException

---
+++ src/main/java/com/example/demo/global/exception/oauth/OAuth2AuthenticationProcessingException.java
@@ -0,0 +1,9 @@
+package com.example.demo.global.exception.oauth;
+
+import javax.naming.AuthenticationException;
+
+public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+    public OAuth2AuthenticationProcessingException(String msg) {
+        super(msg);
+    }
+}
