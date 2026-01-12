fe87ead82f6e9035699ae7e7cd46010789071814
C0ng_yun <s25069@gsm.hs.kr>
2026-01-12 23:48:37 +0900

    add : RefreshTokenRepository 구현

---
+++ src/main/java/com/example/demo/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain.auth.repository;
+
+import com.example.demo.domain.auth.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByMemberId(Long userId);
+}
