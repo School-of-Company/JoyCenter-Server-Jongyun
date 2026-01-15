51bbc19f5f7be9c1b1c58f5e7d02329b883ec1e6
C0ng_yun <s25069@gsm.hs.kr>
2026-01-15 23:23:36 +0900

    add : GoogleOAuth2UserInfo 추가

--- src/main/java/com/example/demo/global/oauth/data/GoogleOAuth2UserInfo.java
+++ src/main/java/com/example/demo/global/oauth/data/GoogleOAuth2UserInfo.java
@@ -1,7 +1,6 @@
 package com.example.demo.global.oauth.data;
 
-import com.example.demo.global.oauth.OAuth2UserInfo;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import com.example.demo.global.oauth.common.OAuthType;
 
 import java.util.Map;
 
@@ -18,8 +17,8 @@ public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
     }
 
     @Override
-    public String getProvider() {
-        return "google";
+    public OAuthType getProvider() {
+        return OAuthType.GOOGLE;
     }
 
     @Override
