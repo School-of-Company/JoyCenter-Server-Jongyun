a2c7ac6df93f4b139effbff919bcd0664888bd0b
C0ng_yun <s25069@gsm.hs.kr>
2026-01-16 07:47:18 +0900

    update : CustomOAuth2UserService 경로수정

--- src/main/java/com/example/demo/global/oauth/OAuth2UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.global.oauth;
-
-
-public interface OAuth2UserInfo {
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getName();
-}
