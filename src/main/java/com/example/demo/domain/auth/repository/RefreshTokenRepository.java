be45e1a1e0db8cdb0f7ccee1dd26696008d84737
C0ng_yun <s25069@gsm.hs.kr>
2026-01-15 23:30:29 +0900

    update : RefreshTokenRepository 메서드 추가

--- src/main/java/com/example/demo/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/demo/domain/auth/repository/RefreshTokenRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByMemberId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
