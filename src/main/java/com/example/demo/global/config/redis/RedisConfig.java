1049d34881d3f53a778eb3d241c35a6bf871f3bc
C0ng_yun <s25069@gsm.hs.kr>
2026-01-18 14:25:06 +0900

    update: 파일 위치 변경

--- src/main/java/com/example/demo/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/demo/global/config/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.global.redis;
+package com.example.demo.global.config.redis;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
