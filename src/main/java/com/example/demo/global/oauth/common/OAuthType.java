92793cef0c2c0218aac2d5b6bcdc17afed84bf9f
C0ng_yun <s25069@gsm.hs.kr>
2026-01-27 13:48:05 +0900

    update: OAuthType 문자열 변환 기능 추가

--- src/main/java/com/example/demo/global/oauth/common/OAuthType.java
+++ src/main/java/com/example/demo/global/oauth/common/OAuthType.java
@@ -1,6 +1,22 @@
 package com.example.demo.global.oauth.common;
 
+import com.example.demo.global.exception.oauth.OAuth2AuthenticationProcessingException;
+
+import java.util.Arrays;
+
 public enum OAuthType {
     GOOGLE,
-    KAKAO,
+    KAKAO;
+
+    public static OAuthType from(String type) {
+        try {
+            return Arrays.stream(values())
+                    .filter(t -> t.name().equalsIgnoreCase(type))
+                    .findFirst()
+                    .orElseThrow(()-> new OAuth2AuthenticationProcessingException("지원하지 않는 제공자입니다."));
+        } catch (OAuth2AuthenticationProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
